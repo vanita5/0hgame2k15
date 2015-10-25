5e25b468cc877256f45a6d66b13ec1ef8d1067d0
vanita5 <mail@vanita5.de>
2015-10-25 02:16:04 +0200

    add water effect

--- js/game.js
+++ js/game.js
@@ -9,8 +9,12 @@ var app = playground({
         y: 10
     },
 
+    water: {
+        visible: false
+    },
+
     create: function() {
-        this.loadImages("poempel", "toilet");
+        this.loadImages("poempel", "toilet", "water");
     },
 
     ready: function() {
@@ -19,13 +23,18 @@ var app = playground({
 
     render: function() {
         this.layer.clear("#000");
+
         this.layer.drawImage(this.images.poempel, this.poempel.x, this.poempel.y);
+        if (this.water.visible) this.layer.drawImage(this.images.water, 285, 50);
+
         this.layer.drawImage(this.images.toilet, 250, 50);
     },
 
     mousemove: function(event) {
         var y = event.y - 75;
         this.poempel.y = y > 90 ? 90 : y;
+
+        this.water.visible = y >= 90;
 
         console.log(event.x);
     }
