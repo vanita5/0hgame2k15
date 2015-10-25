f31b702fb038706e51a186103276bad7c08b9ec1
vanita5 <mail@vanita5.de>
2015-10-25 02:09:20 +0200

    playground init

--- js/game.js
+++ js/game.js
@@ -1,3 +1,21 @@
 var app = playground({
 
+    width: 400,
+    height: 250,
+    scale: 2,
+
+    create: function() {
+        this.loadImages("poempel", "toilet");
+    },
+
+    ready: function() {
+
+    },
+
+    render: function() {
+        this.layer.clear("#000");
+        this.layer.drawImage(this.images.poempel, 285, 10);
+        this.layer.drawImage(this.images.toilet, 250, 50);
+    }
+
 });
